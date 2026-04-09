b031c6f0a2061759540b3220963bc7545e568a31
SantiagoZVcesde <zapatavillasantiago@gmail.com>
2026-04-08 20:53:46 -0500

    (feature) se crea la relacionuno a uno con pacientes y citas

--- src/main/java/com/example/demo_basic/model/entity/Odontologo.java
+++ src/main/java/com/example/demo_basic/model/entity/Odontologo.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -29,4 +31,10 @@ public class Odontologo extends BaseEntity {
 
     @Column(name = "tarjeta_profesional", nullable = false, unique = true)
     private int tarjetaProfesional;
+
+    @ManyToOne
+    private Paciente paciente;
+
+    @ManyToMany
+    private Cita cita;
 }
