baea67fc375ba4379a356aa299c26b4537bd099d
SantiagoZVcesde <zapatavillasantiago@gmail.com>
2026-04-18 23:37:39 -0500

    (Feacture): Se crea un controller para el put

--- src/main/java/com/example/demo_basic/controller/OdontologoController.java
+++ src/main/java/com/example/demo_basic/controller/OdontologoController.java
@@ -1,23 +1,23 @@
 package com.example.demo_basic.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody; 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo_basic.model.entity.OdontologoEntity;
 import com.example.demo_basic.service.OdontologoService;
 
-import org.springframework.web.bind.annotation.RequestBody;
-
 @RestController
 @RequestMapping("/odontologos")
 public class OdontologoController {
+
     @Autowired
     private OdontologoService odontologoService;
 
@@ -34,6 +34,12 @@ public class OdontologoController {
     @GetMapping("/{id}")
     public OdontologoEntity obtener(@PathVariable Long id) {
         return odontologoService.obtenerPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public OdontologoEntity actualizar(@PathVariable Long id, @RequestBody OdontologoEntity odontologo) {
+        odontologo.setId(id);
+        return odontologoService.guardar(odontologo);
     }
 
     @DeleteMapping("/{id}")
